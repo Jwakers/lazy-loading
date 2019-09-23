042caa99973fa96ceb819ea38046f275621d0aef
Jack <jackwakeham82@gmail.com>
2019-09-23 13:41:22 +0100

    Added cloudinary CDN

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-require("@babel/polyfill");
 import 'intersection-observer';
 import 'picturefill';
 import './styles/style.scss';
 import Lazyload from './scripts/modules/lazy-load';
-
 new Lazyload();
--- src/scripts/modules/lazy-load.js
+++ src/scripts/modules/lazy-load.js
@@ -2,21 +2,16 @@ require('intersection-observer');
 
 export default class Lazyload {
     constructor() {
-        console.log('Intersection Observer point 1')
         this.init();
     }
 
     observer() {
-        console.log('Intersection Observer point 2')
         let options = {
-            rootMargin: "-100px 0px"
+            rootMargin: "40px 0px"
         };
         let observer = new IntersectionObserver(entries => {
-            console.log('Intersection Observer point 3')
             entries.forEach(entry => {
-                console.log('Intersection Observer point 4')
                 if (entry.isIntersecting) {
-                    console.log('Intersection Observer point 5')
                     const [src, srcset] = [entry.target.getAttribute('data-src'), entry.target.getAttribute('data-srcset')]
 
                     if (src) entry.target.setAttribute('src', src);
@@ -29,12 +24,10 @@ export default class Lazyload {
     }
 
     init() {
-        console.log('Intersection Observer point 6')
         const observer = this.observer();
         let elements = document.querySelectorAll('.lazyload');
 
         // elements.forEach(el => observer.observe(el))
         for (let index of elements) observer.observe(index);
-        console.log('Intersection Observer point 7')
     }
 }
